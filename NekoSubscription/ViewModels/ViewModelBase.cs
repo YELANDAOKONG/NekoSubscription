7480d81c15c1d6a838bf8cf7ca9e61c463a7a9f3
YELANDAOKONG <116365019+YELANDAOKONG@users.noreply.github.com>
2026-07-15 02:18:31 +0800

    init: add initial NekoSubscription solution with Avalonia UI, Core, and Entities projects

---
+++ NekoSubscription/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace NekoSubscription.ViewModels;
+
+public abstract class ViewModelBase : ObservableObject
+{
+}
---
+++ NekoSubscription/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace NekoSubscription.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
